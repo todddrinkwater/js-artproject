15305a5de80f8203969a3ddb7830cb01f4ac6872
todddrinkwater <todddrinkwaternz@gmail.com>
2017-02-15 20:27:24 +1300

    add stop function to stop tracks when button pressed

--- main.js
+++ main.js
@@ -1,20 +1,9 @@
 var button = document.getElementsByClassName("button");
 var graphic = document.getElementById("graphic");
+var soundArr = [];
 
-graphic.innerHTML = "*";
 
-// Add event to play sound when mouse button is held down on li.
-for (var i = 0; i < button.length; i++){
-  button[i].addEventListener('click', getValue);
-}
 
-function getValue(){
-buttonValue = this.getAttribute("data-buttonValue");
-playSound();
-}
-
-function playSound() {
-console.log("playSound test");
 
 var a = document.getElementById("a");
 var amin = document.getElementById("amin");
@@ -26,6 +15,41 @@ var emin = document.getElementById("emin");
 var f = document.getElementById("f");
 var g = document.getElementById("g");
 
+
+graphic.innerHTML = "*";
+
+// Add event to play sound when mouse button is held down on li.
+for (var i = 0; i < button.length; i++){
+  button[i].addEventListener('click', getValue);
+}
+
+function getValue(){
+buttonValue = this.getAttribute("data-buttonValue");
+stopSound();
+playSound();
+}
+
+function stopSound(){
+  a.pause();
+  a.currentTime = 0;
+  b.pause();
+  b.currentTime = 0;
+  c.pause();
+  c.currentTime = 0;
+  d.pause();
+  d.currentTime = 0;
+  e.pause();
+  e.currentTime = 0;
+  emin.pause();
+  emin.currentTime = 0;
+  f.pause();
+  f.currentTime = 0;
+  g.pause();
+  g.currentTime = 0;
+}
+
+
+function playSound() {
 
   if(buttonValue === "A"){
     a.play();
